8dc26657ce1985fa53badb517150bc04bb6e2124
土肥一郎 <idohi@live.jp>
2018-03-16 01:31:59 +0900

    Initial Commit

---
+++ GenericInstaller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GenericInstaller
+//
+//  Created by 土肥一郎 on 2018/03/16.
+//  Copyright © 2018年 土肥一郎. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GenericInstaller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GenericInstaller
+//
+//  Created by 土肥一郎 on 2018/03/16.
+//  Copyright © 2018年 土肥一郎. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
