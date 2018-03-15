43b3ead2b7ca55e2c9569a7bfcd55e8a212af342
Akira Kido <achts.y@gmail.com>
2018-03-16 02:59:26 +0900

    gui 作成

--- GenericInstaller/ViewController.swift
+++ GenericInstaller/ViewController.swift
@@ -10,10 +10,40 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var targetUrl: NSTextField!
+    @IBAction func onSelectFile(_ sender: NSButton) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.canChooseFiles = true
+        openPanel.canCreateDirectories = false
+        openPanel.allowedFileTypes = ["app"]
+        openPanel.begin(completionHandler: { (result) -> Void in
+            if result == NSApplication.ModalResponse.OK {
+                guard let url = openPanel.url else { return }
+                self.targetUrl.stringValue = url.path
+            }
+        })
+    }
+    
+    @IBOutlet weak var outputUrl: NSTextField!
+    @IBAction func onSelectOutputDirectoryDown(_ sender: NSButton) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = true
+        openPanel.canChooseFiles = false
+        openPanel.begin(completionHandler: { (result) -> Void in
+            if result == NSApplication.ModalResponse.OK {
+                guard let url = openPanel.url else {return}
+                self.outputUrl.stringValue = url.path
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
@@ -23,5 +53,20 @@ class ViewController: NSViewController {
     }
 
 
+}
+
+extension NSView {
+    
+    @IBInspectable var backgroundColor: NSColor? {
+        get {
+            guard let layer = layer, let backgroundColor = layer.backgroundColor else {return nil}
+            return NSColor(cgColor: backgroundColor)
+        }
+        set {
+            wantsLayer = true
+            layer?.backgroundColor = newValue?.cgColor
+        }
+    }
+    
 }
 
